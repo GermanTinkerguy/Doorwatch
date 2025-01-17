9ef81c3ddf354dbdbea42c9006621fc9397a4d95
GermanTinkerGuy <nightwolf_ger@gmx.com>
2025-01-17 09:37:15 +0100

    Update -Readability

--- Firmware/Doorwatch/main.cpp
+++ Firmware/Doorwatch/main.cpp
@@ -6,6 +6,10 @@
  */ 
 
 
+#ifndef F_CPU				// Include guards
+#define F_CPU 8000000UL		// AT90USB162 runs at 8 MHz clock speed - unsigned long
+#endif						// Include guards
+
 #include <stdint.h>			// Integer variable
 #include <avr/io.h>			// I/O ports
 #include <util/delay.h>		// Delay function
@@ -13,127 +17,84 @@
 #include <avr/interrupt.h>	// Interrupt implementation
 #include <avr/wdt.h>		// Watchdog timer
 
-#ifndef F_CPU				// Include guards
-#define F_CPU 8000000UL		// AT90USB162 runs at 8 MHz clock speed - unsigned long
-#endif						// Include guards
+#define BUTTON_PRESSED PINB & (1 << PB4)	// PB4 - Button pressed
+#define LED_GN_ON  PORTB |= (1 << PB5)		// PB5 - LED green on
+#define LED_GN_OFF PORTB &= ~(1 << PB5)		// PB5 - LED green off
+#define LED_RD_ON  PORTB |= (1 << PB6)		// PB6 - LED red on
+#define LED_RD_OFF PORTB &= ~(1 << PB6)		// PB6 - LED red off
 
 
-/***************************************************
- *  Name:        ISR(PCINT0_vect)
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Interrupt service routine
- *
- ***************************************************/
+// Interrupt service routine
 ISR(PCINT0_vect){			// Port B, PCINT0 - PCINT7
 }
 
 
-/***************************************************
- *  Name:        status
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Status indication.
- *
- ***************************************************/
+// Status indication
 void status (){
-	PORTB |= (1 << PB5);
+	LED_GN_ON;
 	_delay_ms (1000);
-	PORTB &= ~(1 << PB5);
+	LED_GN_OFF;
 	_delay_ms (1000);
 }
 
 
-/***************************************************
- *  Name:        open
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Indication for door is open.
- *
- ***************************************************/
+// Indication for door is open
 void open (){
-	PORTB |= (1 << PB6);
+	LED_RD_ON;
 	_delay_ms (1000);
-	PORTB &= ~(1 << PB6);
+	LED_RD_OFF;
 	_delay_ms (1000);
 }
 
 
-/***************************************************
- *  Name:        alarm
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Alarm.
- *
- ***************************************************/
+// Alarm
 void alarm (){
-	PORTB |= (1 << PB5);
-	PORTB |= (1 << PB6);
+	LED_GN_ON;
+	LED_RD_ON;
 	_delay_ms (1000);
-	PORTB &= ~(1 << PB5);
-	PORTB &= ~(1 << PB6);
+	LED_GN_OFF;
+	LED_RD_OFF;
 	_delay_ms (1000);
 }
 
 
-/***************************************************
- *  Name:        main(void)
- *
- *  Returns:     Nothing.
- *
- *  Parameters:  None.
- *
- *  Description: Main program
- *
- ***************************************************/
+// Main program
 int main(void){
 
 		// I/O Pin setup
-		DDRB &= ~(1 << PB4);		// Pin PB4 as input - Button
-		DDRB |= (1 << PB5);			// Pin PB5 as output - green LED
-		DDRB |= (1 << PB6);			// Pin PB6 as output - Buzzer/ red LED
+		DDRB &= ~(1 << PB4);	// PB4 input  0 - Button
+		DDRB |= (1 << PB5);		// PB5 output 1 - LED green
+		DDRB |= (1 << PB6);		// PB6 output 1 - LED red
 
 		// Variables
-		uint8_t counter = 0;		// Variable for counting the clock cycles how long the door was open
+		uint8_t counter = 0;	// Variable for counting the clock cycles how long the door was open
 
 	// Main loop
 	while (1){
 
-		status();											// Status blink
+		status();				// Status blink
 
-		if (PINB & (1 << PB4) && (counter < 2)){			// If button PB4 pressed = door open (HIGH -> LOW)
-			for (uint8_t i = 0; i <= 2; i++){				// Loop until it checks input again (2 times)
-				open();										// Door open indication
+		if (BUTTON_PRESSED && (counter < 2)){			// If button PB4 pressed = door open (HIGH -> LOW)
+			for (uint8_t i = 0; i <= 2; i++){			// Loop until it checks input again (2 times)
+				open();									// Door open indication
 			}
-			counter += 1;									// Counts up for how many times is input "door open" checked
+			counter += 1;								// Counts up for how many times is input "door open" checked
 		}
 
-		if (PINB & (1 << PB4) && (counter == 2)){			// If button PB4 still pressed = door still open (HIGH -> LOW)
-			for (uint8_t i = 0; i <= 2; i++){				// Loop until it checks input again (2 times)
-				alarm();									// Door to long open -> alarm
+		if (BUTTON_PRESSED && (counter == 2)){			// If button PB4 still pressed = door still open (HIGH -> LOW)
+			for (uint8_t i = 0; i <= 2; i++){			// Loop until it checks input again (2 times)
+				alarm();								// Door to long open -> alarm
 			}
 		}
 
-		if ((!(PINB & (1 << PB4))) && (counter <= 2)){		// If button PB4 released = door closed and wasn't open before or closed for a while (LOW -> HIGH)
-			counter = 0;									// Reset counter
+		if ((!(BUTTON_PRESSED)) && (counter <= 2)){		// If button PB4 released = door closed and wasn't open before or closed for a while (LOW -> HIGH)
+			counter = 0;								// Reset counter
 			
 			// Pin change interrupt setup
-			cli();											// Disable interrupt for programming
-			PCICR |= (1<<PCIE0);							// Turn on port b
-			PCMSK0 |= (1 << PB4);							// Turn on pin PB4, which is PCINT4
-			sei();											// Enable interrupt
+			cli();										// Disable interrupt for programming
+			PCICR |= (1<<PCIE0);						// Turn on port b
+			PCMSK0 |= (1 << PB4);						// Turn on pin PB4, which is PCINT4
+			sei();										// Enable interrupt
 
 			// Sleep mode list
 			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -143,7 +104,7 @@ int main(void){
 //			set_sleep_mode(SLEEP_MODE_IDLE);
 //			set_sleep_mode(SLEEP_MODE_ADC);
 
-			sleep_mode();									// Start sleep mode
+			sleep_mode();		// Start sleep mode
 		}
 	}
 }
