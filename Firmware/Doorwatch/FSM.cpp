aae94c1cc3f008b6c9e4c24e11366e45a0f2c907
GermanTinkerguy <159229877+GermanTinkerguy@users.noreply.github.com>
2025-05-20 10:26:46 +0200

    Update FSM.cpp

--- Firmware/Doorwatch/FSM.cpp
+++ Firmware/Doorwatch/FSM.cpp
@@ -23,6 +23,9 @@
 #define LED_RD_OFF		(PORTB &= ~(1 << PB6))	// PB6 - LED red off
 #define LED_RD_TOGGLE	(PORTB ^= (1 << PB6))	// PB6 - LED red toggle
 
+
+volatile uint8_t counter = 0;	// Variable for counting the clock cycles how long the door was open
+
 enum State
 {
 	IDLE,
@@ -32,24 +35,68 @@ enum State
 };
 State currentState = IDLE;
 
-int setup (void)
+
+// Interrupt service routine
+ISR (PCINT0_vect)		// Port B, PCINT0 - PCINT7
+{
+}
+
+// Status indication
+int idle ()
+{
+	LED_GN_ON;
+	_delay_ms (1000);
+	LED_GN_OFF;
+	_delay_ms (1000);
+}
+
+// Indication for door is open
+int open ()
+{
+	LED_RD_ON;
+	_delay_ms (1000);
+	LED_RD_OFF;
+	_delay_ms (1000);
+}
+
+// Alarm
+int alarm ()
+{
+	LED_GN_ON;
+	LED_RD_ON;
+	_delay_ms (1000);
+	LED_GN_OFF;
+	LED_RD_OFF;
+	_delay_ms (1000);
+}
+
+int sleep ()
+{
+	
+}
+
+int setup ()
 {
 	DDRB &= ~(1 << PB4);	// PB4 input  0 - Button
 	DDRB |= (1 << PB5);		// PB5 output 1 - LED green
 	DDRB |= (1 << PB6);		// PB6 output 1 - LED red
 }
 
-int loop (void)
+int loop ()
 {
 	switch (currentState)
 	{
 		case IDLE:
+			idle ();
 		break;
 		case OPEN:
+			open ();
 		break;
 		case ALARM:
+			alarm ();
 		break;
 		case SLEEP:
+			sleep ();
 		break;
 	}	
 }
@@ -61,5 +108,64 @@ int main (void)
 	while (1)
 	{
 		loop ();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+// Main program
+int main (void)
+{
+	// I/O Pin setup
+	DDRB &= ~(1 << PB4);	// PB4 input  0 - Button
+	DDRB |= (1 << PB5);		// PB5 output 1 - LED green
+	DDRB |= (1 << PB6);		// PB6 output 1 - LED red
+
+	// Variables
+	uint8_t counter = 0;	// Variable for counting the clock cycles how long the door was open
+
+	// Main loop
+	while (1)
+	{
+		status ();	// Status blink
+
+		if (BUTTON_PRESSED && (counter < 2))	// If button PB4 pressed = door open (HIGH -> LOW)
+		{
+			for (uint8_t i = 0; i <= 2; i++)	// Loop until it checks input again (2 times)
+			{
+				open();							// Door open indication
+			}
+			counter += 1;						// Counts up for how many times is input "door open" checked
+		}
+
+		if (BUTTON_PRESSED && (counter == 2))	// If button PB4 still pressed = door still open (HIGH -> LOW)
+		{
+			for (uint8_t i = 0; i <= 2; i++)	// Loop until it checks input again (2 times)
+			{
+				alarm();						// Door open too long -> alarm
+			}
+		}
+
+		if ((!(BUTTON_PRESSED)) && (counter <= 2))	// If button PB4 released = door closed and wasn't open before or closed for a while (LOW -> HIGH)
+		{
+			counter = 0;							// Reset counter
+
+			// Pin change interrupt setup
+			cli ();						// Disable status register global interrupt - for programming
+			PCICR |= (1<<PCIE0);		// Enable pin change interrupt PB4 - PCINT4
+			PCMSK0 |= (1 << PCINT4);	// Enable pin change interrupt mask PB4 - PCINT4
+			sei ();						// Enable status register global interrupt
+
+			// Sleep mode list
+			set_sleep_mode (SLEEP_MODE_PWR_DOWN);	// Set sleep mode: power down
+			sleep_mode ();							// Activate sleep mode
+		}
 	}
 }
